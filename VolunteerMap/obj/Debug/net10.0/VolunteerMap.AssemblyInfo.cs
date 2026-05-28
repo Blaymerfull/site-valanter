e759057064c55c49fc455f98a1468062038fa8e4
Blaymerfull <122620941+Blaymerfull@users.noreply.github.com>
2026-05-28 14:18:12 +0700

    теперь правильно работающий скрол в карточках волонтеров

--- VolunteerMap/obj/Debug/net10.0/VolunteerMap.AssemblyInfo.cs
+++ VolunteerMap/obj/Debug/net10.0/VolunteerMap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VolunteerMap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5302c469a07aab49e959386d6060e8286a237f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ac6724b2ef5fa1b31b3fdac7f824ce1470faa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VolunteerMap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VolunteerMap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
